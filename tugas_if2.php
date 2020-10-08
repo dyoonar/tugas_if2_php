9fb171f5ee926c58b2396078b2892ebc0d2f4c98
dyoonar <71246827+dyoonar@users.noreply.github.com>
2020-10-08 12:04:43 +0700

    Add files via upload

---
+++ tugas_if2.php
@@ -0,0 +1,13 @@
+<?php
+$a = 5;
+$b = 5;
+
+
+if($a > $b){
+    echo $a." Lebih besar dari ".$b;
+}else if($a<$b){
+    echo $a." Lebih kecil dari ".$b;
+}else
+    echo $a." sama dengan ".$b;
+
+?>
